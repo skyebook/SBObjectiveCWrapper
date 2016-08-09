ef1074844a1e1ff1b60f794487efb2b094b411d1
Zach Bai <zbai@samsungaccelerator.com>
2016-08-09 11:50:17 -0400

    swift 3 changes

--- sources/SBObjectiveCWrapper.swift
+++ sources/SBObjectiveCWrapper.swift
@@ -11,11 +11,11 @@ import SwiftyBeaver
 
 protocol Loggable: class {
     
-    static func verbose(@autoclosure msg: () -> Any, _ path: String, _ function: String, line: Int)
-    static func debug(@autoclosure msg: () -> Any, _ path: String, _ function: String, line: Int)
-    static func info(@autoclosure msg: () -> Any, _ path: String, _ function: String, line: Int)
-    static func warning(@autoclosure msg: () -> Any, _ path: String, _ function: String, line: Int)
-    static func error(@autoclosure msg: () -> Any, _ path: String, _ function: String, line: Int)
+    static func verbose(_ msg: @autoclosure () -> Any, _ path: String, _ function: String, line: Int)
+    static func debug(_ msg: @autoclosure () -> Any, _ path: String, _ function: String, line: Int)
+    static func info(_ msg: @autoclosure () -> Any, _ path: String, _ function: String, line: Int)
+    static func warning(_ msg: @autoclosure () -> Any, _ path: String, _ function: String, line: Int)
+    static func error(_ msg: @autoclosure () -> Any, _ path: String, _ function: String, line: Int)
     
 }
 
@@ -23,37 +23,37 @@ extension SwiftyBeaver: Loggable {}
 
 public class SBObjectiveCWrapper: NSObject {
     
-    @objc class func _setLogClassForTesting(logClass: AnyObject) {
+    @objc class func _setLogClassForTesting(_ logClass: AnyObject) {
         self.logClass = logClass as! Loggable.Type
     }
     
     static var logClass: Loggable.Type = SwiftyBeaver.self
     
-    public class func logVerbose(message: String, filePath: String, function: String, line: Int) {
+    public class func logVerbose(_ message: String, filePath: String, function: String, line: Int) {
         
         logClass.verbose(message, filePath, function, line: line)
         
     }
     
-    public class func logDebug(message: String, filePath: String, function: String, line: Int) {
+    public class func logDebug(_ message: String, filePath: String, function: String, line: Int) {
         
         logClass.debug(message, filePath, function, line: line)
         
     }
     
-    public class func logInfo(message: String, filePath: String, function: String, line: Int) {
+    public class func logInfo(_ message: String, filePath: String, function: String, line: Int) {
         
         logClass.info(message, filePath, function, line: line)
         
     }
     
-    public class func logWarning(message: String, filePath: String, function: String, line: Int) {
+    public class func logWarning(_ message: String, filePath: String, function: String, line: Int) {
         
         logClass.warning(message, filePath, function, line: line)
         
     }
     
-    public class func logError(message: String, filePath: String, function: String, line: Int) {
+    public class func logError(_ message: String, filePath: String, function: String, line: Int) {
         
         logClass.error(message, filePath, function, line: line)
         
